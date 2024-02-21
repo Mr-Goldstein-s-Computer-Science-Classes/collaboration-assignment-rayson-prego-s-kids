d78262e58669aef0776fd024150c8d110db4b6e8
TylerP123456789 <113116228+TylerP123456789@users.noreply.github.com>
2024-02-21 12:28:54 -0500

    Update Gradebook.java

--- Gradebook.java
+++ Gradebook.java
@@ -1,2 +1,21 @@
 public class Gradebook {
+    public Gradebook(){
+
+    }//default constructor
+
+    double averageGrade;
+    double minimumGrade;
+    double maximumGrade;
+
+    public double getAverageGrade(){
+        return averageGrade;
+    }
+    public double getMinimumGrade(){
+        return minimumGrade;
+    }
+    public double getMaximumGrade(){
+        return maximumGrade;
+    }
+
+
 }
