b71416408af6ed5122babb0f61231aea73174dfc
MattWaldron <126mwaldron@frhsd.com>
2024-02-21 12:46:23 -0500

    Update Student.java

--- Student.java
+++ Student.java
@@ -1,3 +1,35 @@
-public class Student {
+import java.util.ArrayList;
 
+public class Student
+{
+    private String name;
+    private ArrayList<String> assignments;
+
+    private double grade;
+
+    public Student(String name)
+    {
+        this.name = name;
+        this.assignments = new ArrayList<String>();
+    }
+
+    public void addAssignment(String assignName)
+    {
+        assignments.add(assignName);
+    }
+
+    public void gradeAssignment(String assignment, double grade)
+    {
+
+    }
+
+    public double getGrade()
+    {
+        return grade;
+    }
+
+    public void setGrade(double grade)
+    {
+        this.grade = grade;
+    }
 }
