34a6235a2239e686f05976af61df1ea110357d8c
TylerP123456789 <113116228+TylerP123456789@users.noreply.github.com>
2024-02-21 12:42:02 -0500

    Update Gradebook.java

--- Gradebook.java
+++ Gradebook.java
@@ -1,11 +1,24 @@
+import java.util.ArrayList;
 public class Gradebook {
-    public Gradebook(){
-
-    }//default constructor
-
     double averageGradeOverall;
     double minimumGradeOverall;
     double maximumGradeOverall;
+    public ArrayList<String> assignments;
+    public Gradebook(double averageGradeOverall, double minimumGradeOverall, double maximumGradeOverall){
+    this.assignments = new ArrayList<String>();
+    this.averageGradeOverall = averageGradeOverall;
+    this.minimumGradeOverall = minimumGradeOverall;
+    this.maximumGradeOverall = maximumGradeOverall;
+
+    }//default constructor
+
+
+    public void createStudent(String name){
+
+    }
+    public void addAssignments(String assignment){
+        assignments.add(assignment);
+    }
 
     public double getAverageGradeOverall(){
         return averageGradeOverall;
@@ -16,7 +29,7 @@ public class Gradebook {
     public double getMaximumGradeOverall(){
         return maximumGradeOverall;
     }
-    
+
 
 
 }
