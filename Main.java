7e8ad7da840754a55aa56d6b5d060010d3353183
TylerP123456789 <113116228+TylerP123456789@users.noreply.github.com>
2024-02-22 13:56:32 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,22 @@
 public class Main {
     public static void main(String[] args)
     {
+        Student student1 = new Student("Tyler");
+        Student student2 = new Student("James");
+        Student student3 = new Student("Ethan");
+        Student student4 = new Student("Scotty");
+        Student student5 = new Student("David");
+        Student student6 = new Student("Patrick");
+
+        Gradebook gradebook1 = new Gradebook("Stacks and Queues Station Activity");
+        Gradebook gradebook2 = new Gradebook("Stacks and queues programs");
+        Gradebook gradebook3 = new Gradebook("Stacks and queues quiz");
+        Gradebook gradebook4 = new Gradebook("Github Exploration");
+
+
+
+
+
 
     }
 }
